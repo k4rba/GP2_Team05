099c9b084cac3204932045daacd375b0a05d14e6
addeponkeN <addeponken@hotmail.com>
2023-02-01 12:56:20 +0100

    fixed enemy prefab

--- Team05/Assets/Personal/Andreas/Scripts/Actors/Enemy.cs
+++ Team05/Assets/Personal/Andreas/Scripts/Actors/Enemy.cs
@@ -10,7 +10,7 @@ namespace Personal.Andreas.Scripts.Actors
             get => transform.position;
             set => transform.position = value;
         }
-        
+
         public Vector3 FlowDirection { get; set; }
 
         private Rigidbody _body;
@@ -20,13 +20,26 @@ namespace Personal.Andreas.Scripts.Actors
             _body = gameObject.GetComponent<Rigidbody>();
         }
 
+        private void Confused()
+        {
+            var rotationDir = new Vector3(0, Random.Range(200, 1000), 0);
+            var rotationEuler = Quaternion.Euler(rotationDir * Time.fixedDeltaTime);
+            _body.MoveRotation(_body.rotation * rotationEuler);
+        }
+
         public void Move(Vector2 direction)
         {
             var tf = _body.transform;
-            
+
+            if(direction == Vector2.zero)
+            {
+                Confused();
+                return;
+            }
+
             //  todo - take speed from some stat collection
             float speed = 5f;
-            
+
             var pos = tf.position;
             var dir3 = new Vector3(direction.x, 0, direction.y);
             var velocity = dir3 * (speed * Time.deltaTime);
