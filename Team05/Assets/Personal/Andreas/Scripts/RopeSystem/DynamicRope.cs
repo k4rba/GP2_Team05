89ab52830818cc5990496dff148a8cd9980e2e7d
addeponkeN <addeponken@hotmail.com>
2023-02-07 12:44:23 +0100

    rope progress

--- Team05/Assets/Personal/Andreas/Scripts/RopeSystem/DynamicRope.cs
+++ Team05/Assets/Personal/Andreas/Scripts/RopeSystem/DynamicRope.cs
@@ -8,15 +8,14 @@ namespace Andreas.Scripts
 {
     public class DynamicRope : MonoBehaviour
     {
-        [SerializeField] private Transform start, end;
-
+        [SerializeField] private Rigidbody start, end;
         [SerializeField] private int segmentCount;
         [SerializeField] private GameObject prefSegment;
-        [SerializeField] private GameObject prefHinge;
 
         private List<GameObject> _segments;
 
         [SerializeField] private bool generate;
+        [SerializeField] private float segmentSize = 1f;
 
         private void Awake()
         {
@@ -38,21 +37,41 @@ namespace Andreas.Scripts
             {
                 Destroy(_segments[i]);
             }
+
             _segments.Clear();
         }
-        
+
         private void GenerateRope()
         {
+            Clear();
+
             var pos = transform.position;
+            var parentPos = start.position;
+            
+            var startPos = parentPos;
+            
             for(int i = 0; i < segmentCount; i++)
             {
-                var seg = Instantiate(prefSegment, pos, Quaternion.identity, transform);
+                var segPos = new Vector3(startPos.x, startPos.y - (i * segmentSize), startPos.z);
+                var seg = Instantiate(prefSegment, segPos, Quaternion.identity, transform);
+                seg.transform.eulerAngles = new Vector3(0, 0, 0);
                 _segments.Add(seg);
 
                 seg.name = $"Segment {i}";
-                
+
+                var joint = seg.GetComponent<CharacterJoint>();
+                if(i == 0)
+                {
+                    // Destroy(joint);
+                    // seg.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeAll;
+                    joint.connectedBody = start;
+                }
+                else
+                {
+                    var parent = _segments[i - 1];
+                    joint.connectedBody = parent.GetComponent<Rigidbody>();
+                }
             }
         }
-
     }
 }
