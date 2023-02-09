463f81bf1800a033ca7b8d068a6a0a67533dc61b
karba <karbalive@hotmail.com>
2023-02-09 17:59:53 +0100

    Base version of all player abilities added
    and integrates properly with enemies

--- Team05/Assets/Personal/Andreas/Scripts/Projectile.cs
+++ Team05/Assets/Personal/Andreas/Scripts/Projectile.cs
@@ -1,48 +1,47 @@
 ﻿using System;
 using UnityEngine;
 
-namespace Andreas.Scripts
-{
-    public enum Alliances
-    {
+namespace Andreas.Scripts {
+    public enum Alliances {
         None,
         Player,
         Evil,
     }
 
-    public class Projectile : MonoBehaviour
-    {
+    public class Projectile : MonoBehaviour {
         public Alliances Alliance;
-        public int Damage;
+        public float Damage;
         public float Speed;
 
         private float _lifeTime = 6f;
 
         private Vector3 _direction;
 
-        public void Init(Vector3 targetPosition)
-        {
+        public void Init(Vector3 targetPosition) {
             _direction = (targetPosition - transform.position).normalized;
         }
 
-        private void Update()
-        {
+        private void Update() {
             var dt = Time.deltaTime;
             var velocity = _direction * (Speed * dt);
             transform.position += velocity;
 
             _lifeTime -= dt;
-            if(_lifeTime <= 0)
+            if (_lifeTime <= 0)
                 Destroy(gameObject);
         }
 
-        public void OnHitReceiver()
-        {
+        public void OnHitReceiver() {
             Destroy(gameObject);
         }
 
-        private void OnTriggerEnter(Collider other)
-        {
+        private void OnTriggerEnter(Collider other) {
+            if (other.GetComponent<MeleeAttacks>() != null) {
+                var attackType = other.gameObject.GetComponent<MeleeAttacks>().meleeAttackType;
+                if (attackType == MeleeAttacks.MeleeAttackType.MeleeDome) {
+                    Destroy(gameObject);
+                }
+            }
         }
     }
 }
