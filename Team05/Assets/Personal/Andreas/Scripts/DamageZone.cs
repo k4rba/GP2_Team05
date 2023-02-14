7cff4475c54ed854f2f27e17870aefb93592e527
addeponkeN <addeponken@hotmail.com>
2023-02-14 18:16:53 +0100

    added deathzone prefab

---
+++ Team05/Assets/Personal/Andreas/Scripts/DamageZone.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+namespace Andreas.Scripts
+{
+    public class DamageZone : MonoBehaviour
+    {
+        public float Damage = 999f;
+
+        private void Awake()
+        {
+            
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            var player = other.GetComponent<Player>();
+            if(player != null)
+            {
+                player.Health.InstantDamage(player, Damage);
+            }
+        }
+        
+    }
+}
