8e6a17753baf667d3263cb7a7e4fb1de43afbbf7
addeponkeN <addeponken@hotmail.com>
2023-02-15 22:57:56 +0100

    ropestuff

--- Team05/Assets/Personal/Andreas/Scripts/RopeSystem/RopeStates/RopeStateFlow.cs
+++ Team05/Assets/Personal/Andreas/Scripts/RopeSystem/RopeStates/RopeStateFlow.cs
@@ -1,5 +1,8 @@
-﻿using Andreas.Scripts.RopeSystem.SegmentStates;
+﻿using System.Drawing;
+using Andreas.Scripts.RopeSystem.SegmentStates;
+using UnityEngine;
 using Util;
+using Color = UnityEngine.Color;
 
 namespace Andreas.Scripts.RopeSystem.RopeStates
 {
@@ -11,9 +14,21 @@ namespace Andreas.Scripts.RopeSystem.RopeStates
         private int _index;
         private Timer _timer;
 
+        private GameObject _objLight;
+
         public override void Start()
         {
             base.Start();
+            // _objLight = new GameObject("RopeLight");
+            var lightPrefab = FastResources.Load<GameObject>("Prefabs/Rope/RopeLight"); 
+            _objLight = Object.Instantiate(lightPrefab, Rope.transform);
+            
+            // var light = _objLight.AddComponent<Light>();
+            // light.color = Color.white;
+            // light.type = LightType.Point;
+            // light.intensity = 5f;
+            // light.range = 20f;
+            // light.shadows = LightShadows.Soft;
             _timer = Rope.Data.TransferSpeed / Rope.Segments.Count;
         }
 
@@ -36,9 +51,18 @@ namespace Andreas.Scripts.RopeSystem.RopeStates
         public void SetRopeCool()
         {
             var segment = Rope.Segments[_index].GetComponent<RopeSegment>();
+            segment.GetComponent<Rigidbody>()
+                .AddForce(Random.insideUnitSphere * Rng.NextF(3f, 10f), ForceMode.VelocityChange);
             segment.AddState(new RopeSegmentStateColor());
             segment.AddState(new RopeSegmentStateSize());
+
+            _objLight.transform.position = segment.transform.position;
         }
-        
+
+        public override void Exit()
+        {
+            base.Exit();
+            Object.Destroy(_objLight);
+        }
     }
 }
